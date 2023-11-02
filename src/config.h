ebc7b64110b8a7596909e3158a707c70de1a56a6
huben1337 <88612147+huben1337@users.noreply.github.com>
2023-11-02 17:15:11 +0100

    config file added

---
+++ src/config.h
@@ -0,0 +1,38 @@
+#include <stdint.h>
+// ####################################### Serial Config #######################################
+
+#define UART_BAUDRATE 250000    // baudrate
+#define ACK_MESSAGE             // define to send ACK
+
+// calculated values
+#define BAUD_PRESCALE (((F_CPU / (UART_BAUDRATE * 16UL))) - 1)
+
+// ######################################## Port Config ########################################
+
+// Servo Step Control
+#define SERVO_STEP_CONTROL_PORT PORTD
+#define SERVO_PULSE_PIN PD6    // needs to be on SERVO_STEP_CONTROL_PORT
+#define SERVO_DIR_PIN PD7      // needs to be on SERVO_STEP_CONTROL_PORT
+
+// Servo General Control
+#define SERVO_GENERAL_CONTROL_PORT PORTB
+#define SERVO_ENABLE_PIN PB5   // needs to be on PORTB
+
+// Set Port Config
+#define DDRB_CONFIG DDRB | (1 << SERVO_ENABLE_PIN)                          // all pins on PORTB
+#define DDRD_CONFIG DDRD | (1 << SERVO_PULSE_PIN) | (1 << SERVO_DIR_PIN)    // all pins on PORTD
+// #define DDRC_CONFIG DDRC | <WHATEVER_PINS>                                // all pins on PORTC
+
+// ###################################### Servo Config #########################################
+
+#define SERVO_RPM (float) 3000
+#define SERVO_SPR (float) 10000                 // servo steps per rotation
+#define SERVO_GEAR_RATIO (float) 3              // servo gear ratio
+#define ACTUATOR_MAX_LENGTH (float) 696         // fully extended length in mm
+#define SCREW_LEAD (uint8_t) 10                 // screw lead in mm (lead = (pitch / number of screws))
+#define INPUT_RESOLUTION  (uint8_t) 10          // input resolution in bits (max 16)
+
+// calculated values
+typedef uint8_t factor_t;                       // type of factor (float is more precise but slower)
+#define STEP_FACTOR (factor_t) (( (SERVO_SPR / SERVO_GEAR_RATIO) * (ACTUATOR_MAX_LENGTH / SCREW_LEAD) ) / (1 << INPUT_RESOLUTION))
+#define SERVO_PULSE_PERIOD (uint8_t) (F_CPU / ( (SERVO_RPM / 60) * (SERVO_SPR / SERVO_GEAR_RATIO) ))
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,31 @@
 #include <stdint.h>
+#include <config.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define ACK_MESSAGE true
-
-#define SERVO_PULSE_PERIOD 96U
-
-#define UART_BAUDRATE 250000
-#define BAUD_PRESCALE (((F_CPU / (UART_BAUDRATE * 16UL))) - 1)
 
 volatile __uint24 currentStepPosition = 0; // __uint24
 volatile __uint24 targetStepPosition = 0; // __uint24
 
 
 int main (void) {
-    DDRB |= (1 << PB7);
-    DDRD |= (1 << PD6) | (1 << PD7);
+    #ifdef DDRB_CONFIG
+        DDRB = DDRB_CONFIG;
+    #endif
+    #ifdef DDRD_CONFIG
+        DDRD = DDRD_CONFIG;
+    #endif
+    #ifdef DDRC_CONFIG
+        DDRC = DDRC_CONFIG;
+    #endif
 
-    PORTB = PORTB & (1 << PB5);
+    SERVO_GENERAL_CONTROL_PORT |= (1 << SERVO_ENABLE_PIN); // Enable Servo
 
     cli();
     TCCR2A = (1 << WGM21);
     TCCR2B = (1 << CS20);
     TCNT2 = 0;
-    OCR2A = SERVO_PULSE_PERIOD - 1; // 149 is possible
-    OCR2B = (SERVO_PULSE_PERIOD / 2) - 1; // 
+    OCR2A = SERVO_PULSE_PERIOD - 1;
+    OCR2B = (SERVO_PULSE_PERIOD / 2) - 1;
     TIMSK2 |= (1 << OCIE2A) | (1 << OCIE2B);
     sei();
 
@@ -33,41 +35,24 @@ int main (void) {
 
     while (!(UCSR0A & (1 << UDRE0)));
     UDR0 = 'C';
-    // while (!(UCSR0A & (1 << UDRE0)));
-
-    // option 1: =((10000/3)*(696/10))/2^10
-    const float factor = 226.5625; 
-
-    // option 2: =((10000/3)*696)/2^8
-
-    // volatile bool firstByteRead = false;
-    uint8_t firstByte = 0;
 
     while (true) {
-        if (UCSR0A & (1 << RXC0)) { // uart0_AvailableBytes()
-            if (firstByte) {
-                targetStepPosition = ((uint16_t) (firstByte << 8) | UDR0) * factor;
-                // firstByteRead = false;
-                firstByte = 0;
-                #if ACK_MESSAGE
-                    // uart_putc_noblock('R');
-                    while (!(UCSR0A & (1 << UDRE0)));
-                    UDR0 = 'R';
-                    // while (!(UCSR0A & (1 << UDRE0)));
-                #endif
-            } else {
-                firstByte = UDR0;
-                // firstByteRead = true;
-            }
-        }
+        while (!(UCSR0A & (1 << RXC0)));
+        uint8_t firstByte = UDR0;
+        while (!(UCSR0A & (1 << RXC0)));
+        targetStepPosition = (__uint24) ((uint16_t) (firstByte << 8) | UDR0) * STEP_FACTOR;
+        #ifdef ACK_MESSAGE
+            while (!(UCSR0A & (1 << UDRE0)));
+            UDR0 = 'R';
+        #endif
     }
 }
 
 ISR(TIMER2_COMPA_vect) {
     asm volatile (
-        "cbi %[portd], 6"
+        "cbi %[port], %[servo_pulse_pin]\n\t"
         :
-        : [portd] "I" (_SFR_IO_ADDR(PORTD))
+        : [port] "I" (_SFR_IO_ADDR(SERVO_STEP_CONTROL_PORT)), [servo_pulse_pin] "I" (SERVO_PULSE_PIN)
     );
 }
 
@@ -80,77 +65,21 @@ ISR(TIMER2_COMPB_vect) {
         "breq end\n\t"
         "brlo up\n\t"
 
-        "sbi %[portd], 7\n\t"
-        "sbi %[portd], 6\n\t"
+        "sbi %[port], %[servor_dir_pin]\n\t"
+        "sbi %[port], %[servo_pulse_pin]\n\t"
         "sbiw %[currentStepPosition], 1\n\t"
         "sbc %C[currentStepPosition], __zero_reg__\n\t"
         "rjmp end\n\t"
         
         "up:\n\t"
-            "cbi %[portd], 7\n\t"
-            "sbi %[portd], 6\n\t"
+            "cbi %[port], %[servor_dir_pin]\n\t"
+            "sbi %[port], %[servo_pulse_pin]\n\t"
             "adiw %[currentStepPosition], 1\n\t"
             "adc %C[currentStepPosition], __zero_reg__\n\t"
         
         "end:\n\t"
         : [currentStepPosition] "+r" (currentStepPosition)
-        : [portd] "I" (_SFR_IO_ADDR(PORTD)), [targetStepPosition] "r" (targetStepPosition)
+        : [port] "I" (_SFR_IO_ADDR(SERVO_STEP_CONTROL_PORT)), [servo_pulse_pin] "I" (SERVO_PULSE_PIN), [servor_dir_pin] "I" (SERVO_DIR_PIN), [targetStepPosition] "r" (targetStepPosition)
     );
 
-    /* asm volatile (
-        "cp %A[currentStepPosition], %A[targetStepPosition]\n\t"
-        "cpc %B[currentStepPosition], %B[targetStepPosition]\n\t"
-        "breq end\n\t"
-        "brlo up\n\t"
-
-        "sbi %[portd], 7\n\t"
-        "sbi %[portd], 6\n\t"
-        "sbiw %[currentStepPosition], 1\n\t"
-        "rjmp end\n\t"
-        
-        "up:\n\t"
-            "cbi %[portd], 7\n\t"
-            "sbi %[portd], 6\n\t"
-            "adiw %[currentStepPosition], 1\n\t"
-        
-        "end:\n\t"
-        : [currentStepPosition] "+w" (currentStepPosition)
-        : [portd] "I" (_SFR_IO_ADDR(PORTD)), [targetStepPosition] "w" (targetStepPosition)
-    ); */
-    
-    /* if (targetStepPosition > currentStepPosition) {
-        asm volatile(
-            "cbi %[portd], 7\n\t"
-            "sbi %[portd], 6\n\t"
-            "adiw %[currentStepPosition], 1"
-            : [currentStepPosition] "+r" (currentStepPosition)
-            : [portd] "I" (_SFR_IO_ADDR(PORTD))
-        );
-    } else if (targetStepPosition < currentStepPosition) {
-        asm volatile(
-            "sbi %[portd], 7\n\t"
-            "sbi %[portd], 6\n\t"
-            "sbiw %[currentStepPosition], 1"
-            : [currentStepPosition] "+r" (currentStepPosition)
-            : [portd] "I" (_SFR_IO_ADDR(PORTD))
-        );
-    } */
-
-    /* if (targetStepPosition > currentStepPosition) {
-        currentStepPosition++;
-        asm volatile(
-            "cbi %[portd], 7\n\t"
-            "sbi %[portd], 6"
-            : 
-            : [portd] "I" (_SFR_IO_ADDR(PORTD))
-        );
-    } else if (targetStepPosition < currentStepPosition) {
-        currentStepPosition--;
-        asm volatile(
-            "sbi %[portd], 7\n\t"
-            "sbi %[portd], 6"
-            : 
-            : [portd] "I" (_SFR_IO_ADDR(PORTD))
-        );
-    } */
 }
